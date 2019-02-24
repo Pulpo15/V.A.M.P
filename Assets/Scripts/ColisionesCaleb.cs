f32d062fd22b860247aed63bb95b1d7ffca75a9a
Pulpo <pol.gomar@enti.cat>
2019-02-24 03:30:14 +0100

    V.A.M.P-0.2
    Versión de mejora con las colisiones de los cubos, trasladado el código de estos a un script propio asignado a cada Obj.

--- Assets/Scripts/ColisionesCaleb.cs
+++ Assets/Scripts/ColisionesCaleb.cs
@@ -6,91 +6,21 @@ public class ColisionesCaleb : MonoBehaviour {
 
     public Rigidbody2D Caleb;
     public Rigidbody2D Cubo;
-    public Collider2D CuboCollider;
     Vector2 v2;
     public float salto;
     public float speed;
-    bool IsTouchingCubo = false;
 
     void Start () {
 		
 	}
 	
-	// Update is called once per frame
 	void Update () {
         Moving();
 	}
 
     private void OnCollisionStay2D(Collision2D collision)
     {
-        if (collision.gameObject.name == "Cubo")
-        {
-            ColisionCubo(collision);
-        }
-    }
 
-    void ColisionCubo(Collision2D collision)
-    {
-        if (Input.GetKey(KeyCode.E) == false && IsTouchingCubo == false)
-        { 
-            if (Caleb.transform.eulerAngles == new Vector3(0, 0, 0))
-            {
-                collision.transform.position = new Vector3(Caleb.position.x, Caleb.position.y + 1);
-                collision.transform.eulerAngles = new Vector3(Caleb.transform.eulerAngles.x, Caleb.transform.eulerAngles.y, Caleb.transform.eulerAngles.z);
-                IsTouchingCubo = true;
-            }
-            else if (Caleb.transform.eulerAngles == new Vector3(0, 0, 180))
-            {
-                collision.transform.position = new Vector3(Caleb.position.x, Caleb.position.y - 1f);
-                collision.transform.eulerAngles = new Vector3(Caleb.transform.eulerAngles.x, Caleb.transform.eulerAngles.y, Caleb.transform.eulerAngles.z);
-                IsTouchingCubo = true;
-            }
-            else if (Caleb.transform.eulerAngles == new Vector3(0, 0, 90))
-            {
-                collision.transform.position = new Vector3(Caleb.position.x - 1, Caleb.position.y);
-                collision.transform.eulerAngles = new Vector3(Caleb.transform.eulerAngles.x, Caleb.transform.eulerAngles.y, Caleb.transform.eulerAngles.z);
-                IsTouchingCubo = true;
-            }
-            else 
-            {
-                collision.transform.position = new Vector3(Caleb.position.x + 1, Caleb.position.y);
-                collision.transform.eulerAngles = new Vector3(Caleb.transform.eulerAngles.x, Caleb.transform.eulerAngles.y, Caleb.transform.eulerAngles.z);
-                IsTouchingCubo = true;
-
-            }
-        }
-        else if (Input.GetKey(KeyCode.E) == true)
-        {
-            Cubo.velocity = new Vector3(0, 0, 0);
-            Caleb.velocity = new Vector3(0, 0, 0);
-            IsTouchingCubo = false;
-            if (Caleb.transform.eulerAngles == new Vector3(0, 0, 0))
-            {
-                collision.transform.position = new Vector3(Caleb.position.x, Caleb.position.y + 1.1f);
-                collision.transform.eulerAngles = new Vector3(Caleb.transform.eulerAngles.x, Caleb.transform.eulerAngles.y, Caleb.transform.eulerAngles.z);
-
-            }
-            else if (Caleb.transform.eulerAngles == new Vector3(0, 0, 180))
-            {
-                collision.transform.position = new Vector3(Caleb.position.x, Caleb.position.y - 1.1f);
-                collision.transform.eulerAngles = new Vector3(Caleb.transform.eulerAngles.x, Caleb.transform.eulerAngles.y, Caleb.transform.eulerAngles.z);
-            }
-            else if (Caleb.transform.eulerAngles == new Vector3(0, 0, 90))
-            {
-                collision.transform.position = new Vector3(Caleb.position.x - 1.1f, Caleb.position.y);
-                collision.transform.eulerAngles = new Vector3(Caleb.transform.eulerAngles.x, Caleb.transform.eulerAngles.y, Caleb.transform.eulerAngles.z);
-            }
-            else
-            {
-                collision.transform.position = new Vector3(Caleb.position.x + 1.1f, Caleb.position.y);
-                collision.transform.eulerAngles = new Vector3(Caleb.transform.eulerAngles.x, Caleb.transform.eulerAngles.y, Caleb.transform.eulerAngles.z);
-            }
-        }
-    }
-
-    void PosicionCubo (int direccion)
-    {
-       
     }
 
     void Moving()
---
+++ Assets/Scripts/Cubo.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cubo : MonoBehaviour {
+
+    public Rigidbody2D Caleb;
+    public Rigidbody2D Cubos;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Caleb")
+        {
+            ColisionCaleb();
+        }
+    }
+
+
+    void ColisionCaleb()
+    {
+        if (Input.GetKey(KeyCode.E) == false)
+        {
+            if (Caleb.transform.eulerAngles == new Vector3(0, 0, 0))
+            {
+                gameObject.transform.position = new Vector3(Caleb.position.x, Caleb.position.y + 1);
+                gameObject.transform.eulerAngles = new Vector3(Caleb.transform.eulerAngles.x, Caleb.transform.eulerAngles.y, Caleb.transform.eulerAngles.z);
+            }
+            else if (Caleb.transform.eulerAngles == new Vector3(0, 0, 180))
+            {
+                gameObject.transform.position = new Vector3(Caleb.position.x, Caleb.position.y - 1f);
+                gameObject.transform.eulerAngles = new Vector3(Caleb.transform.eulerAngles.x, Caleb.transform.eulerAngles.y, Caleb.transform.eulerAngles.z);
+            }
+            else if (Caleb.transform.eulerAngles == new Vector3(0, 0, 90))
+            {
+                gameObject.transform.position = new Vector3(Caleb.position.x - 1, Caleb.position.y);
+                gameObject.transform.eulerAngles = new Vector3(Caleb.transform.eulerAngles.x, Caleb.transform.eulerAngles.y, Caleb.transform.eulerAngles.z);
+            }
+            else
+            {
+                gameObject.transform.position = new Vector3(Caleb.position.x + 1, Caleb.position.y);
+                gameObject.transform.eulerAngles = new Vector3(Caleb.transform.eulerAngles.x, Caleb.transform.eulerAngles.y, Caleb.transform.eulerAngles.z);
+
+            }
+        }
+        else if (Input.GetKey(KeyCode.E) == true)
+        {
+            Cubos.velocity = new Vector3(0, 0, 0);
+            Caleb.velocity = new Vector3(0, 0, 0);
+            if (Caleb.transform.eulerAngles == new Vector3(0, 0, 0))
+            {
+                gameObject.transform.position = new Vector3(Caleb.position.x, Caleb.position.y + 1.1f);
+                gameObject.transform.eulerAngles = new Vector3(Caleb.transform.eulerAngles.x, Caleb.transform.eulerAngles.y, Caleb.transform.eulerAngles.z);
+
+            }
+            else if (Caleb.transform.eulerAngles == new Vector3(0, 0, 180))
+            {
+                gameObject.transform.position = new Vector3(Caleb.position.x, Caleb.position.y - 1.1f);
+                gameObject.transform.eulerAngles = new Vector3(Caleb.transform.eulerAngles.x, Caleb.transform.eulerAngles.y, Caleb.transform.eulerAngles.z);
+            }
+            else if (Caleb.transform.eulerAngles == new Vector3(0, 0, 90))
+            {
+                gameObject.transform.position = new Vector3(Caleb.position.x - 1.1f, Caleb.position.y);
+                gameObject.transform.eulerAngles = new Vector3(Caleb.transform.eulerAngles.x, Caleb.transform.eulerAngles.y, Caleb.transform.eulerAngles.z);
+            }
+            else
+            {
+                gameObject.transform.position = new Vector3(Caleb.position.x + 1.1f, Caleb.position.y);
+                gameObject.transform.eulerAngles = new Vector3(Caleb.transform.eulerAngles.x, Caleb.transform.eulerAngles.y, Caleb.transform.eulerAngles.z);
+            }
+        }
+    }
+}
