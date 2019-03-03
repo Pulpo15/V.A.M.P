24027d11317549b29ba035a60f8b6e08cf528b84
KiSungJi <andrea.succhi@enti.cat>
2019-03-03 17:20:02 +0100

    V.A.M.P-0.4.1.4
    Lamia persigue a Caleb y colisiona con los cubos

---
+++ Assets/Scripts/lamiaScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lamiaScript : MonoBehaviour {
+
+    public float speed;
+
+    private Transform target;
+    
+    // Use this for initialization
+	void Start () {
+
+		target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //cuando caleb toque algo, lamia persigue
+
+        Perseguir();
+
+
+    }
+    
+
+    void Perseguir ()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+    }
+
+
+}
