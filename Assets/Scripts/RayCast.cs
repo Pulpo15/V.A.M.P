3c84ba3ff957592e9e86bcd04c0c4d76ddc56eb5
Pulpo <pol.gomar@enti.cat>
2019-03-15 11:51:21 +0100

    V.A.M.P-0.4.1.5
    Mapa Modificado

---
+++ Assets/Scripts/RayCast.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCast : MonoBehaviour {
+
+    public Transform RCBeg, RCEnd;
+
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	void Raycasting ()
+    {
+        Debug.DrawLine(RCBeg.position, RCEnd.position, Color.black);
+    }
+	// Update is called once per frame
+	void Update () {
+
+        Raycasting();
+	}
+}
