681a9e212fa8948ff4b751844f6d241013cdecf4
KiSungJi <andrea.succhi@enti.cat>
2019-03-20 00:36:15 +0100

    V.A.M.P-0.4.1.8
    Poner el texto de la historia con el trigger (que no funciona).

---
+++ Assets/Scripts/Dialogo/MainDialogo.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainDialogo : MonoBehaviour {
+
+    public Text nameText;
+    public Text dialogoText;
+
+    private Queue<string> sentences;
+
+    // Use this for initialization
+    void Start() {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogo(Dialogo dialogo)
+    {
+        nameText.text = dialogo.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogo.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        ShowNextSentence();
+    }
+
+    public void ShowNextSentence()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (sentences.Count == 0)
+            {
+                EndDialogo();
+                return;
+            }
+            string sentence = sentences.Dequeue();
+            dialogoText.text = sentence;
+        }
+        
+        
+    }
+
+    void EndDialogo()
+    {
+        Debug.Log("Fin de texto");
+    }
+
+  
+    
+}
---
+++ Assets/Scripts/Dialogo/Trigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+
+    public Dialogo dialogo;
+
+
+    public void OnTriggerStay2D(Collider2D other)
+    {
+        FindObjectOfType<MainDialogo>().StartDialogo(dialogo);
+    }
+
+}
