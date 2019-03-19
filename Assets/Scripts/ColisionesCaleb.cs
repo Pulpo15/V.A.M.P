d48dd9cf50eb3d483cbe013441db1808299e6484
Pulpo <pol.gomar@enti.cat>
2019-03-20 00:03:50 +0100

    V.A.M.P-0.4.1.7
    Dash añadido
    Rata añadida
    Humano añadido
    Interacción con lamia añadida (Contiene Bugs)

--- Assets/Scripts/ColisionesCaleb.cs
+++ Assets/Scripts/ColisionesCaleb.cs
@@ -7,31 +7,141 @@ public class ColisionesCaleb : MonoBehaviour {
     public Rigidbody2D Caleb;
     public Rigidbody2D Cubo;
     public Animator animacion;
+    public Collider2D Rata;
+    public Collider2D Humano;
+    public SpriteRenderer SpriteRata;
+    public SpriteRenderer SpriteHumano;
     Vector2 v2;
     public float salto;
     public float speed;
+    public float vida;
+    public static float vidaParaLamia;
+    public int reputacion = 0;
     bool isTouching = false;
     bool isWalking = false;
+    public float dashSpeed;
+    private float dashTime;
+    public float startDashTime;
+    private int direction;
+    public float startDashDeelay;
+    private float dashDeelay = 0;
+    public static bool isDashing = false;
+    public static float vidaRecibidaDeLamia;
+
+
+
 
     void Start() {
-
+        dashTime = startDashTime;
     }
 
     void Update() {
         Moving();
+        Dash();
+        vidaParaLamia = vida;
+        vidaRecibidaDeLamia = lamiaScript.vidaCaleb;
+
+    }
+
+    void Dash(){
+        dashDeelay -= Time.deltaTime;
+        if (direction == 0)
+        {
+            if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.Space) && dashDeelay <= 0)
+            {
+                direction = 1;
+                dashDeelay = startDashDeelay;
+            }
+            else if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.Space) && dashDeelay <= 0)
+            {
+                direction = 2;
+                dashDeelay = startDashDeelay;
+            }
+            else if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.Space) && dashDeelay <= 0)
+            {
+                direction = 3;
+                dashDeelay = startDashDeelay;
+            }
+            else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.Space) && dashDeelay <= 0)
+            {
+                direction = 4;
+                dashDeelay = startDashDeelay;
+            }
+        }
+        else
+        {
+            if (dashTime <= 0)
+            {
+                direction = 0;
+                dashTime = startDashTime;
+                Caleb.velocity = Vector2.zero;
+                isDashing = false;
+            }
+            else
+            {
+                dashTime -= Time.deltaTime;
+                if (direction == 1)
+                {
+                    Caleb.velocity = Vector2.left * dashSpeed;
+                    isDashing = true;
+                }
+                else if (direction == 2)
+                {
+                    Caleb.velocity = Vector2.right * dashSpeed;
+                    isDashing = true;
+                }
+                else if (direction == 3)
+                {
+                    Caleb.velocity = Vector2.up * dashSpeed;
+                    isDashing = true;
+                }
+                else if (direction == 4)
+                {
+                    Caleb.velocity = Vector2.down * dashSpeed;
+                    isDashing = true;
+                }
+            }
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-
     }
 
     private void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.gameObject.name == "Cubo")
         {
+            
             speed = 1.5f;
             isTouching = true;
+        }
+        else if (collision.gameObject.name == "Rata")
+        {
+            if (Input.GetMouseButton(0))
+            {
+                print("+10 de vida");
+                vida = vida + 10;
+                reputacion = reputacion - 5;
+                Rata.enabled = false;
+                SpriteRata.enabled = false;
+            }
+        }
+        else if (collision.gameObject.name == "Humano")
+        {
+            if (Input.GetMouseButton(0))
+            {
+                print("+20 de vida");
+                vida = vida + 25;
+                reputacion = reputacion + 20;
+                Humano.enabled = false;
+                SpriteHumano.enabled = false;
+            }
+        }
+        else if (collision.gameObject.name == "Lamia")
+        {
+            print("-15 de vida");
+            vida = vidaRecibidaDeLamia;
         }
     }
 
--- Assets/Scripts/lamiaScript.cs
+++ Assets/Scripts/lamiaScript.cs
@@ -5,31 +5,80 @@ using UnityEngine;
 public class lamiaScript : MonoBehaviour {
 
     public float speed;
-
+    public Rigidbody2D Lamia;
     private Transform target;
+    private static bool isDashing;
+    public static float vidaCaleb;
+    private bool cantMove = false;
+    public float startTimeToWait;
+    private float timeToWait = 0f;
+    public Collider2D ColliderLamia;
+    public SpriteRenderer SpriteRenderLamia;
+    public float startAttackDeelay;
+    private float attackDeelay = 0f;
+
     
     // Use this for initialization
 	void Start () {
-
 		target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-
-
+        timeToWait = startTimeToWait;
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
         //cuando caleb toque algo, lamia persigue
-
         Perseguir();
+        isDashing = ColisionesCaleb.isDashing;
+        vidaCaleb = ColisionesCaleb.vidaParaLamia;
+        
+        attackDeelay -= Time.deltaTime;
+    }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Caleb")
+        {
+            if (isDashing == true)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, target.position, 0);
+                Lamia.bodyType = RigidbodyType2D.Static;
+                cantMove = true;
+            }
+
+        }
 
     }
-    
 
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Caleb")
+        {
+            if (Input.GetMouseButton(0) && cantMove == true)
+            {
+                ColliderLamia.enabled = false;
+                SpriteRenderLamia.enabled = false;
+            }
+            else if (attackDeelay <= 0f)
+            {
+                print("Ataque");
+                vidaCaleb = vidaCaleb - 15f;
+                attackDeelay = startAttackDeelay;
+            }
+        }
+    }
     void Perseguir ()
     {
-        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        timeToWait -= Time.deltaTime;
+        if (cantMove == false)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+        else if (timeToWait <= 0)
+        {
+            cantMove = false;
+            timeToWait = startTimeToWait;
+        }
     }
 
 
