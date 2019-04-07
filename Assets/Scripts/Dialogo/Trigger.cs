74378386a9b949aaf8f73476a5a14bb58319a6ce
Pulpo <pol.gomar@enti.cat>
2019-04-07 02:41:17 +0200

    V.A.M.P-0.4.1.13

--- Assets/Scripts/Dialogo/Trigger.cs
+++ Assets/Scripts/Dialogo/Trigger.cs
@@ -10,16 +10,44 @@ public class Trigger : MonoBehaviour
     public Canvas Texto;
     public Text VarTexto;
     public Text VarTitulo;
+    public bool isOnText = false;
+    public float timeToWait = 10.0f;
+    private float timeToWaitCur;
+    public int Dialog = 0;
 
 
     private void Start()
     {
         Texto.enabled = false;
+        timeToWaitCur = timeToWait;
+        isOnText = true;
     }
 
     private void Update()
     {
-        
+
+        ShowText();
+    }
+
+    void ShowText()
+    {
+        timeToWaitCur -= Time.deltaTime;
+        if (isOnText == true && Dialog == 0 && timeToWaitCur == 0)
+        {
+            VarTexto.text = "¿Donde estoy? ¿Cuanto tiempo he dormido?";
+            Texto.enabled = true;
+            Dialog = 1;
+            if (Input.GetKey(KeyCode.Return) && Dialog == 1 && isOnText == true)
+            {
+                VarTexto.text = "¿Y mis padres? Me encuentro fatal.";
+                Dialog = 2;
+            }
+        }
+        else if (Input.GetKey(KeyCode.Return) && Dialog == 2 && isOnText == true)
+        {
+            Texto.enabled = false;
+            isOnText = false;
+        }
     }
 
     void OnTriggerEnter2D(Collider2D col)
@@ -27,8 +55,8 @@ public class Trigger : MonoBehaviour
         if (col.gameObject.name == "Diario")
         {
             //FindObjectOfType<MainDialogo>().StartDialogo(dialogo);
-            VarTexto.text = "asd";
-            Texto.enabled = true;
+            isOnText = true;
+
         }
     }
 
